fe960a8e8dd46688ec0031a6e533d96f41d4e04e
Alex Kempton <alex@blackdiamond.co.uk>
2015-11-12 23:57:26 +0000

    gulp concat stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 'use strict';
 
-var gulp = require('gulp');
+var gulp = require('gulp'),
+	concat = require('gulp-concat');
 
-gulp.task('hello', function() {
-	console.log('Hello!');
+gulp.task('concatScripts', function() {
+	gulp.src([
+		'bower_components/jquery/dist/jquery.js',
+		'js/modules/_header.js'
+	]).pipe(concat('app.js'))
+	.pipe(gulp.dest('js'));
 });
 
 gulp.task('default', ['hello'], function() {
