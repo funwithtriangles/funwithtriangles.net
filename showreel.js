c7665df7096b2ec1ddf6450cc8af61c455523070
Alex Kempton <alex@funwithtriangles.net>
2021-03-29 15:45:05 +0200

    Showreel generation

---
+++ showreel.js
@@ -0,0 +1,30 @@
+const editly = require("editly")
+
+const clips = [
+  "octo",
+  "werm",
+  "rbl",
+  "ghosts-dev",
+  "rainbow",
+  "tape",
+  "stigmergy",
+  "smoke",
+]
+
+const config = {
+  outPath: "public/showreel.mp4",
+  width: 512,
+  height: 512,
+  clips: clips.map((item) => ({
+    transition: null,
+    layers: [
+      {
+        type: "video",
+        resizeMode: "cover",
+        path: `clips/${item}.mp4`,
+      },
+    ],
+  })),
+}
+
+editly(config).catch(console.error)
